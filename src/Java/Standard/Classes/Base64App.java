a2c38837ecb22d14f7b29c7291b352b271110193
Faza Lanang <faza.lanangdjati@gmail.com>
2022-10-21 17:07:40 +0700

    base64 class

---
+++ src/Java/Standard/Classes/Base64App.java
@@ -0,0 +1,17 @@
+package Java.Standard.Classes;
+
+import java.util.Base64;
+
+public class Base64App {
+    public static void main(String[] args) {
+        String original = "Faza Lanang Djati";
+
+        String encoded = Base64.getEncoder().encodeToString(original.getBytes());
+        System.out.println(encoded);
+
+        byte[] bytes = Base64.getDecoder().decode(encoded);
+        String result = new String(bytes);
+
+        System.out.println(result);
+    }
+}
