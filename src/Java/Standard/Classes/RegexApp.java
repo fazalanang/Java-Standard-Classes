bfec3ce3666e18e508ae2d72b5e88d861f3fe34b
Faza Lanang <faza.lanangdjati@gmail.com>
2022-10-24 14:44:05 +0700

    regular expression

---
+++ src/Java/Standard/Classes/RegexApp.java
@@ -0,0 +1,18 @@
+package Java.Standard.Classes;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexApp {
+    public static void main(String[] args) {
+        String name = "Faza Lanang Djati Matajari Mitra Solusi";
+
+        Pattern pattern = Pattern.compile("[a-zA-Z]*[a][a-zA-Z]*");
+        Matcher matcher = pattern.matcher(name);
+
+        while (matcher.find()){
+            String result = matcher.group();
+            System.out.println(result);
+        }
+    }
+}
