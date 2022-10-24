b98ccb86609b0c94ef4c854a60656052edc3abf8
Faza Lanang <faza.lanangdjati@gmail.com>
2022-10-24 13:27:14 +0700

    properties class

---
+++ src/Java/Standard/Classes/PropertiesApp.java
@@ -0,0 +1,44 @@
+package Java.Standard.Classes;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesApp {
+    public static void main(String[] args) {
+        try {
+            Properties properties = new Properties();
+            properties.load(new FileInputStream("application.properties"));
+
+            String host = properties.getProperty("database.host");
+            String port = properties.getProperty("database.port");
+            String username = properties.getProperty("database.username");
+            String password = properties.getProperty("database.password");
+
+            System.out.println(host);
+            System.out.println(port);
+            System.out.println(username);
+            System.out.println(password);
+
+        } catch (FileNotFoundException exception) {
+            System.out.println("File not found");
+        } catch (IOException exception) {
+            System.out.println("fail load data from file");
+        }
+
+        try {
+            Properties properties = new Properties();
+            properties.put("name.first", "Faza");
+            properties.put("name.middle", "Lanang");
+            properties.put("name.last", "Djati");
+
+            properties.store(new FileOutputStream("name.properties"),"konfigurasi Nama");
+        } catch (FileNotFoundException exception) {
+            System.out.println("Error membuat file properties");
+        } catch (IOException exception) {
+            System.out.println("Error menyimpan properties");
+        }
+    }
+}
