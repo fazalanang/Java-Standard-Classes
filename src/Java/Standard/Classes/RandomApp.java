3c903a3add4d8e1915f80c3167c38f8131711cc2
Faza Lanang <faza.lanangdjati@gmail.com>
2022-10-24 10:08:48 +0700

    random class

---
+++ src/Java/Standard/Classes/RandomApp.java
@@ -0,0 +1,15 @@
+package Java.Standard.Classes;
+
+import java.util.Random;
+
+public class RandomApp {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        for (int i = 0; i < 10 ; i++) {
+            int value = random.nextInt(1000);
+            System.out.println(value);
+        }
+    }
+}
